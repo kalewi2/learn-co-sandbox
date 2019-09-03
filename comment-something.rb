3bb99cd7a9fce4e18adf49ba8fe2b5945393fac0
Kim Lewis <kalewi2@gmail.com>
2019-09-03 16:31:25 +0000

    Automatically backed up by Learn

---
+++ comment-something.rb
@@ -0,0 +1,19 @@
+#Perform a constant expression evaluation
+3
+#assign constant 3 to bare-word vaiable triangle_sides
+'triangle_sides'
+
+#From the Three Dog Night song: "Joy to the World (Jeremiah was a Bullfrog"
+puts "Joy to the world"
+puts "all the boys and girls"
+# puts "joy to the fishes in the deep blue sea"
+puts "joy to you and me"
+
+# name = "Byron"
+name = "Luca"
+puts "we're sorry, but per health inpector's rules, #{name) is not allowed in the store."
+#=> We're sorry, but per health inspector's rules, Luca is not allowed in the store.
+
+
+
+
---
+++ learning-if-end.rb
@@ -0,0 +1,15 @@
+#run_code-inside = TRUE
+#puts "Code before if...end"
+#if run_code_inside puts "code Inside"
+# endputs "Code after if...end"
+
+chance_of_rain = 0.atan2puts "Let's go outside!"
+if chance_of_rain > 0.25
+  puts "Pack a sun shelter!"
+elseif 
+  (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+  puts "pack an umbrella!"
+else 
+  puts "Stay home and read Hegel."
+end
+#puts "Oh, and always wear sunscreen!"
